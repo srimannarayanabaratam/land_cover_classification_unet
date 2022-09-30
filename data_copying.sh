b93f3c1369d44250bdd0a1da3cbc960694bcf82f
Tayyab Mujahid <tayyab.mujahid@gmail.com>
2022-09-30 23:54:55 +0300

    correct minor error in script"

--- data_copying.sh
+++ data_copying.sh
@@ -1,6 +1,6 @@
-!cp $DATA_ROOT/deepglobe2018-landcover-segmentation-traindataset.zip $DATA_DEST
-!unzip /content/land_cover_classification_unet/data/deepglobe2018-landcover-segmentation-traindataset.zip -d $DATA_DEST
-!unzip -qqq /content/land_cover_classification_unet/data/data.zip -d /content/land_cover_classification_unet/data
-%cd $WORKSPACE_ROOT
-!mv  -v data/data/* data/
-!rm -d data/data/
+cp $DATA_ROOT/deepglobe2018-landcover-segmentation-traindataset.zip $DATA_DEST
+unzip /content/land_cover_classification_unet/data/deepglobe2018-landcover-segmentation-traindataset.zip -d $DATA_DEST
+unzip -qqq /content/land_cover_classification_unet/data/data.zip -d /content/land_cover_classification_unet/data
+cd $WORKSPACE_ROOT
+mv  -v data/data/* data/
+rm -d data/data/

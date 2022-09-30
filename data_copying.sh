e5b1a6225e17d728090799aebbd4e331ebd567da
Tayyab Mujahid <tayyab.mujahid@gmail.com>
2022-09-30 23:59:13 +0300

    correct minor error- added paths

--- data_copying.sh
+++ data_copying.sh
@@ -1,6 +1,6 @@
-DATA_ROOT = '/content/drive/MyDrive/fsdl_project'
-DATA_DEST = '/content/land_cover_classification_unet/data'
-WORKSPACE_ROOT = '/content/land_cover_classification_unet'
+DATA_ROOT='/content/drive/MyDrive/fsdl_project'
+DATA_DEST='/content/land_cover_classification_unet/data'
+WORKSPACE_ROOT='/content/land_cover_classification_unet'
 cp $DATA_ROOT/deepglobe2018-landcover-segmentation-traindataset.zip $DATA_DEST
 unzip /content/land_cover_classification_unet/data/deepglobe2018-landcover-segmentation-traindataset.zip -d $DATA_DEST
 unzip -qqq /content/land_cover_classification_unet/data/data.zip -d /content/land_cover_classification_unet/data

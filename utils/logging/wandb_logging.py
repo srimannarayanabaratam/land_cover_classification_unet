5d9f9e8d2998578659a3fd6fbef68bf30f75d1ef
Tayyab Mujahid <tayyab.mujahid@gmail.com>
2022-10-01 10:57:53 +0300

    add name generation method

--- utils/logging/wandb_logging.py
+++ utils/logging/wandb_logging.py
@@ -8,20 +8,18 @@ class Wandblogger:
                    name = name)
         self.job_type = job_type
         self.log_dict = {}
-        self.wandb, self.wandb_current_run = wandb, wandb.run
+
     def log(self,log_dict):
         # if self.wandb_current_run:
         for key, value in log_dict.items():
             self.log_dict[key] = value
     def end_epoch(self):
-        if self.wandb_current_run:
-            wandb.log(self.log_dict)
-            self.log_dict = {}
+        wandb.log(self.log_dict)
+        self.log_dict = {}
 
     def end_batch(self):
-        if self.wandb_current_run:
-            wandb.log(self.log_dict)
-            self.log_dict = {}
+        wandb.log(self.log_dict)
+        self.log_dict = {}
 
 def generate_run_name(self):
     from datetime import datetime
